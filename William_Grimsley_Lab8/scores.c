1cadca52e878785b90d15b59fd60f081c4bfc7fa
wkgrimsley <grimsleybill@ku.edu>
2025-10-15 12:34:48 -0500

    Fix score calculation and improve input validation

--- William_Grimsley_Lab8/scores.c
+++ William_Grimsley_Lab8/scores.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 int main(void){
     int n=0;
     while (n!=1){
@@ -9,15 +8,20 @@ int main(void){
         if (n==1){
             break;
         }
-        printf("Possible combinations is a team's score is %d:\n", n);
+        else if (n<1){
+            printf("Invalid score\n");
+        }
+        else{
+            printf("Possible combinations if a team's score is %d:\n", n);
 
-        for (int td2pt=0; td2pt*8<=n; td2pt++){
-            for (int td1pt=0; td1pt*7<=n; td1pt++){
-                for (int td=0; td*6<=n; td++){
-                    for (int fdg=0; fdg*3<=n; fdg++){
-                        for (int sft=0; sft*2<=n; sft++){
-                            if(td2pt*8+td1pt*7+td*6+fdg*3+sft*2==n){
-                                printf("%2d TD+2pt, %2d TD+FG, %2d TD, %2d 3pt FG, %2d Safety\n", td2pt, td1pt, td, fdg, sft);
+            for (int td2pt=0; td2pt*8<=n; td2pt++){
+                for (int td1pt=0; td1pt*7<=n; td1pt++){
+                    for (int td=0; td*6<=n; td++){
+                        for (int fdg=0; fdg*3<=n; fdg++){
+                            for (int sft=0; sft*2<=n; sft++){
+                                if(td2pt*8+td1pt*7+td*6+fdg*3+sft*2==n){
+                                    printf("%2d TD+2pt, %2d TD+FG, %2d TD, %2d 3pt FG, %2d Safety\n", td2pt, td1pt, td, fdg, sft);
+                                }
                             }
                         }
                     }
